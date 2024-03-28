96b81d277b885ca94641304a0dd433d0a030b131
Amadeus <evan@Amadeus.local>
2024-03-28 15:34:48 -0700

    added csc and csr

--- benchmark/mode_generic.py
+++ benchmark/mode_generic.py
@@ -37,15 +37,32 @@ def example_sparse_tensor(spark_util: SparkUtil):
     print(torch_sparse.to_dense())
     print(torch_result_sparse.to_dense())
 
+def example_csr_tensor(spark_util: SparkUtil):
+    indices = np.array([[0, 0, 3], [4, 0, 0], [0, 6, 0]])
+    sparse_csr = create_csr_from_ndarray(indices)
+    print(sparse_csr)
+    t_id = insert_sparse_tensor(spark_util, sparse_csr)
+    print(t_id)
+    tensor = find_sparse_tensor_by_id(spark_util, t_id)
+    print(tensor)
+
+def example_csc_tensor(spark_util: SparkUtil):
+    indices = np.array([[0, 0, 3], [4, 0, 0], [0, 6, 0]])
+    sparse_csc = create_csc_from_ndarray(indices)
+    print(sparse_csc)
+    t_id = insert_sparse_tensor(spark_util, sparse_csc)
+    print(t_id)
+    tensor = find_sparse_tensor_by_id(spark_util, t_id)
+    print(tensor)
 
 def benchmark_uber_dataset(spark_util: SparkUtil):
-    uber_sparse_tensor = np.loadtxt("dataset/uber/uber.tns", dtype=int).transpose()
+    uber_sparse_tensor = np.loadtxt("/Users/evan/Downloads/dataset/uber/uber.tns", dtype=int).transpose()
     indices = uber_sparse_tensor[0:-1]
     values = uber_sparse_tensor[-1]
     dense_shape = (183, 24, 1140, 1717)
     sparse = SparseTensorCOO(indices, values, dense_shape)
     start = time.time()
-    t_id = insert_sparse_tensor(spark_util, sparse, block_shape=(4, 4))
+    t_id = insert_sparse_tensor(spark_util, sparse, block_shape=(2, 2))
     print(f"Tensor insertion time: {time.time() - start}")
     print(t_id)
     start = time.time()
@@ -59,10 +76,13 @@ if __name__ == '__main__':
     spark_util = SparkUtil()
 
     # Test for dense tensor
-    example_dense_tensor(spark_util)
+    #example_dense_tensor(spark_util)
 
     # Test for sparse tensor
-    example_sparse_tensor(spark_util)
+    #example_sparse_tensor(spark_util)
 
     # Test for uber set
-    benchmark_uber_dataset(spark_util)
+    #benchmark_uber_dataset(spark_util)
+    # Test for csr and csc
+    example_csr_tensor(spark_util)
+    example_csc_tensor(spark_util)
