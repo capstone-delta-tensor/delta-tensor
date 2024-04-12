2c820ab67b61591c43ef1f7b6cef369f7d8616a6
920fandanny <920fandanny@gmail.com>
2024-04-12 12:42:43 -0700

    uncomment a test

--- test/test_coo.py
+++ test/test_coo.py
@@ -12,23 +12,23 @@ class TestCOO(unittest.TestCase):
         self.spark_util = SparkUtil()
         self.delta_tensor = DeltaTensor(self.spark_util)
 
-    # def test_example_sparse_tensor(self):
-    #     print("======================================")
-    #     print("COO example for sparse tensor")
-    #     indices = np.array([[0, 1, 1, 1],
-    #                         [2, 0, 2, 1],
-    #                         [0, 1, 2, 1]])
-    #     values = np.array([3, 4, 5, -1])
-    #     dense_shape = (2, 4, 3)
-    #     sparse = SparseTensorCOO(indices, values, dense_shape)
-    #     t_id = self.delta_tensor.save_sparse_tensor(
-    #         sparse, layout=SparseTensorLayout.COO)
-    #     tensor = self.delta_tensor.get_sparse_tensor_by_id(
-    #         t_id, layout=SparseTensorLayout.COO)
-    #     order = np.ravel_multi_index(sparse.indices, sparse.dense_shape).argsort()
-    #     sparse.indices = sparse.indices[:, order]
-    #     sparse.values = sparse.values[order]
-    #     self.assertTrue(sparse == tensor)
+    def test_example_sparse_tensor(self):
+        print("======================================")
+        print("COO example for sparse tensor")
+        indices = np.array([[0, 1, 1, 1],
+                            [2, 0, 2, 1],
+                            [0, 1, 2, 1]])
+        values = np.array([3, 4, 5, -1])
+        dense_shape = (2, 4, 3)
+        sparse = SparseTensorCOO(indices, values, dense_shape)
+        t_id = self.delta_tensor.save_sparse_tensor(
+            sparse, layout=SparseTensorLayout.COO)
+        tensor = self.delta_tensor.get_sparse_tensor_by_id(
+            t_id, layout=SparseTensorLayout.COO)
+        order = np.ravel_multi_index(sparse.indices, sparse.dense_shape).argsort()
+        sparse.indices = sparse.indices[:, order]
+        sparse.values = sparse.values[order]
+        self.assertTrue(sparse == tensor)
 
     def test_example_sparse_tensor_slicing(self) -> None:
         print("==============================================")
